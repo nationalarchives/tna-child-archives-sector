cb99004154eb9950cde7a869bf1317974445a705
grange85 <andy@grange85.co.uk>
2016-05-06 12:23:58 +0100

    set site variables

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-Child theme template
+## Child theme for the archives sector section of The National Archives web site
--- functions.php
+++ functions.php
@@ -1,71 +1,48 @@
 <?php
-/*
- *
- * ================================================================================================
- *              Removing Metaboxes & Post Tags from Child Theme
- * ================================================================================================
- *
- */
 
-
-// REMOVE POST META BOXES
-if (!function_exists('remove_page_metaboxes')) {
-    function remove_page_metaboxes() {
-        remove_meta_box('commentstatusdiv', 'page', 'normal'); // Comments Metabox
-        remove_meta_box('trackbacksdiv', 'page', 'normal'); // Talkback Metabox
-        remove_meta_box('slugdiv', 'page', 'normal'); // Slug Metabox
-        remove_meta_box('authordiv', 'page', 'normal'); // Author Metabox
-        remove_meta_box('postimagediv', 'page', 'normal'); // Featured Image Metabox
-        remove_meta_box('postcustom', 'page', 'normal');
-        remove_meta_box('commentsdiv', 'page', 'normal');
+// For breadcrumbs and URLs
+// Edit as required
+function tnatheme_globals() {
+    global $pre_path;
+    global $pre_crumbs;
+    // If internal TNA
+    if (substr($_SERVER['REMOTE_ADDR'], 0, 3) === '10.') {
+        $pre_path = '';
+        $pre_crumbs = array(
+            'Site home title' => '/'
+        );
+    // If external TNA
+    } else {
+        $pre_crumbs = array(
+            'About' => '/',
+            'Site home title' => '/archives-sector/'
+        );
+        $pre_path = '/about/archives-sector';
     }
 }
-add_action('admin_menu','remove_page_metaboxes');
+// If web development machine
+if ( $_SERVER['SERVER_ADDR'] !== $_SERVER['REMOTE_ADDR'] ) {
+        tnatheme_globals();
+    } else {
+        $pre_path = '';
+        $pre_crumbs = array(
+            'Site home title' => '/'
+    );
+}
 
+// Dequeue parent styles for re-enqueuing in the correct order
+function dequeue_parent_style() {
+    wp_dequeue_style('tna-styles');
+    wp_deregister_style('tna-styles');
+}
+add_action( 'wp_enqueue_scripts', 'dequeue_parent_style', 9999 );
+add_action( 'wp_head', 'dequeue_parent_style', 9999 );
 
-
-/*
- *
- * ================================================
- *    Override unneeded functions from parent
- * ================================================
- *
- */
-
-
-function education_resource_init () {
-	// remove
+// Enqueue styles in correct order
+function tna_child_styles() {
+    wp_register_style( 'tna-parent-styles', get_template_directory_uri() . '/css/base-sass.css.min', array(), EDD_VERSION, 'all' );
+    wp_register_style( 'tna-child-styles', get_stylesheet_directory_uri() . '/style.css', array(), '0.1', 'all' );
+    wp_enqueue_style( 'tna-parent-styles' );
+    wp_enqueue_style( 'tna-child-styles' );
 }
-function my_add_excerpts_to_pages () {
-	// remove
-}
-function create_post_type () {
-	// remove
-}
-function create_post_type2 () {
-	// remove
-}
-function get_indicator () {
-	// remove
-}
-function get_glossary () {
-	// remove
-}
-function guidance_init () {
-	// remove
-}
-function m_explode () {
-	// remove
-}
-function banner_messages () {
-	// remove
-}
-function create_events_cpt () {
-	// remove
-}
-function include_template_function () {
-	// remove
-}
-function flexslider_shortcode () {
-	// remove
-}
+add_action( 'wp_enqueue_scripts', 'tna_child_styles' );
